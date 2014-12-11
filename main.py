8718ea13541d18b96b446d459fea8fbe917f49dd
Juuso <juuso.karlstrom@gmail.com>
2014-12-12 01:33:42 +0200

    Etenee, hitaasti mutta varmasti

--- main.py
+++ main.py
@@ -8,20 +8,36 @@ Written by Juuso Karlström in 2014
 
 '''
 
-# 1. Select size (freely selectable)
-# 2. Select difficulty (easy,medium, difficult)
-# 3. Print the layout and coordinates
-# 4. Wait till the user
+from random import randrange
 
-def draw_board(x, y):
-    vertical = [x]
-    horizontal = [y]
-    print horizontal, vertical
+"""
 
-    for i in x :
-        print "| |"
+    Set True for debugging mode, False on normal use
+
+"""
+
+DEBUG = True
+
+"""
+
+    Difficulty levels and mine
+    ratio obtained from:
+    http://www.minesweepers.org/playing.asp
+
+"""
+
+BEGINNER = 0.15625
+INTERMEDIARY = 0.15625
+EXPERT = 0.20625
+
+"""
+
+"""
 
 def define_board():
+
+    print " --------SIZE-------------"
+
     while True:
         try:
             size = raw_input("Insert the size of the Minefield (number x number)\nSize: ")
@@ -57,46 +73,117 @@ def define_board():
             continue
 
 def create_board(horizontal, vertical):
-    # Create a gaming board from the user inputted
-    # values (x, y)
+
     board = []
-    for x in range(horizontal):
-        board.append([" "] * vertical)
+
+    """
+
+        Create the Gaming board according to users
+        selections
+
+    """
+    for x in range(vertical):
+        board_row = []
+        for y in range(horizontal):
+            #board_row.append((x, y))
+            board.append(board_row)
+
+    if DEBUG:
+        print "DEBUG_BOARD:", board
+
     return board
 
+def inject_mines_to_board(board, amount_Of_Mines):
+
+    i = 0
+
+    while i <= amount_Of_Mines:
+        j = randrange(len(board))
+        k = randrange(len(board[0]))
+
+        # Check whether that spot already contains a mine
+        if board[j][k] == "X":
+            continue
+        else:
+            board[j][k].append("X")
+            pass
+
+        i += i + 1
+
 def print_board(board):
-    # Add mines later
+
     width = len(board)
     height = len(board[0])
-    columnCount = width + width
+
+    if DEBUG:
+        print "DEBUG_BOARD WIDTH: ", width
+        print "DEBUG_BOARD HEIGHT ", height
 
     print "\n"
 
-    # Print the Horizontal (y)
-    for i in range(width):
+    # Print the Vertical (x)
+    for i in range(height):
         if i == 0:
             print "      " + str(i + 1) + "",
         else:
             print "   " + str(i + 1) + " ",
     print "\n"
 
-    # Print the Vertical (x)
-    for z in range(height):
-        if z != (height -1):
+    # Print the Horizontal (y)
+    for z in range(width):
+
+        if z != (width -1):
             print " " + str(z + 1) + " "
             print "    " + ((6 * "-") * width)
-        elif z == (height -1):
+        elif z == (width -1):
             print " " + str(z +1) + " "
 
+        # for k in range(width):
+        #     if k == 0:
+        #         print sum(board[z][k])
+        #     elif k == width:
+        #         print sum(board[z][k])
+        #     else:
+        #         print sum(board[z][k]), " | ",
+    # Print the values and cell dividers
 
+def choose_difficulty():
 
+    print " --------DIFFICULTY-------------"
+    print " (b) BEGINNER"
+    print " (i) INTERMEDIARY"
+    print " (e) EXPERT"
 
+    while True:
+        choice = raw_input("\nPlease choose difficulty level (b, i, e): ")
+
+        if choice == "b":
+            return BEGINNER
+        elif choice == "i":
+            return INTERMEDIARY
+        elif choice == "e":
+            return EXPERT
+        else:
+            print "Err: Invalid value, please follow the instructions."
+            continue
+
+def calculate_number_of_mines(difficulty, board_Size):
+    return int(difficulty * (len(board_Size) * len(board_Size[0])))
 
 def main():
     # Main
-    x, y = define_board()
+    if DEBUG:
+        x = 10
+        y = 10
+    else:
+        x, y = define_board()
+
     board = create_board(x, y)
+    difficultyLevel = choose_difficulty()
+    mine_Count = calculate_number_of_mines(difficultyLevel, board)
     print_board(board)
+    inject_mines_to_board(board, mine_Count)
+
 
 if __name__ == "__main__":
     main()
